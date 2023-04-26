59cd0f64413dd3dbeeb17a5098cf7bcc00e8f514
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2023-04-26 17:36:11 +0300

    add map in StateTripFragment.kt

--- app/src/main/java/edu/timurmakhmutov/bottomnavstrip/MainActivity.java
+++ app/src/main/java/edu/timurmakhmutov/bottomnavstrip/MainActivity.java
@@ -1,31 +1,20 @@
 package edu.timurmakhmutov.bottomnavstrip;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
-
-import com.android.volley.toolbox.DiskBasedCache;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.yandex.mapkit.MapKitFactory;
 
-import java.util.List;
 import java.util.Objects;
 
-import edu.timurmakhmutov.bottomnavstrip.DataBase.TableForDB;
 import edu.timurmakhmutov.bottomnavstrip.DataBase.TableForDBRepository;
 import edu.timurmakhmutov.bottomnavstrip.databinding.ActivityMainBinding;
-import okhttp3.Cache;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
         BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.homeFragment, R.id.LKFragment, R.id.stateTripFragment, R.id.paymentTripFragment)
                 .build();
@@ -49,11 +36,5 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         tableForDBRepository = new TableForDBRepository(getApplication());
-//        tableForDBRepository.getAllTables().observe(this, new Observer<List<TableForDB>>() {
-//            @Override
-//            public void onChanged(List<TableForDB> tableForDBS) {
-//
-//            }
-//        });
     }
 }
--- app/src/main/java/edu/timurmakhmutov/bottomnavstrip/StateTripFragment.kt
+++ app/src/main/java/edu/timurmakhmutov/bottomnavstrip/StateTripFragment.kt
@@ -1,11 +1,16 @@
 package edu.timurmakhmutov.bottomnavstrip
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
 import edu.timurmakhmutov.bottomnavstrip.databinding.FragmentStateTripBinding
 
 class StateTripFragment : Fragment() {
@@ -14,6 +19,7 @@ class StateTripFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        MapKitFactory.initialize(context)
         // Inflate the layout for this fragment
         fragmentStateTripBinding = FragmentStateTripBinding.inflate(inflater, container, false)
         fragmentStateTripBinding.stateTripEnd.setOnClickListener {
@@ -22,5 +28,25 @@ class StateTripFragment : Fragment() {
             ).navigate(R.id.action_stateTripFragment_to_homeFragment)
         }
         return fragmentStateTripBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentStateTripBinding.mapview.map.move(
+            CameraPosition(Point(55.751999, 37.617734), 14.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 0f),
+            null)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MapKitFactory.getInstance().onStop()
     }
 }
