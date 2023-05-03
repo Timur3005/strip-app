889b6f8245bd65f223fdff1327d097a382338ca4
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2023-05-03 22:01:22 +0300

    delete mapkit using

--- app/src/main/java/edu/timurmakhmutov/bottomnavstrip/MyApp.kt
+++ app/src/main/java/edu/timurmakhmutov/bottomnavstrip/MyApp.kt
@@ -1,11 +1,9 @@
 package edu.timurmakhmutov.bottomnavstrip
 
 import android.app.Application
-import com.yandex.mapkit.MapKitFactory
 
 class MyApp: Application() {
     override fun onCreate() {
         super.onCreate()
-        MapKitFactory.setApiKey("4e7a4456-36a5-46f5-8d1c-7452d3557ba9")
     }
 }
--- app/src/main/java/edu/timurmakhmutov/bottomnavstrip/StateTripFragment.kt
+++ app/src/main/java/edu/timurmakhmutov/bottomnavstrip/StateTripFragment.kt
@@ -1,45 +1,28 @@
 package edu.timurmakhmutov.bottomnavstrip
 
-import android.annotation.SuppressLint
 import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation.findNavController
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.yandex.mapkit.*
-import com.yandex.mapkit.directions.DirectionsFactory
-import com.yandex.mapkit.directions.driving.*
-import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.layers.ObjectEvent
-import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.IconStyle
-import com.yandex.mapkit.map.MapObjectCollection
-import com.yandex.mapkit.map.PlacemarkMapObject
-import com.yandex.mapkit.user_location.UserLocationObjectListener
-import com.yandex.mapkit.user_location.UserLocationView
-import com.yandex.runtime.Error
-import com.yandex.runtime.image.ImageProvider
 import edu.timurmakhmutov.bottomnavstrip.DataBase.TableForDBRepository
 import edu.timurmakhmutov.bottomnavstrip.databinding.FragmentStateTripBinding
-import kotlinx.android.synthetic.main.fragment_state_trip.*
+import edu.timurmakhmutov.bottomnavstrip.lk.LKViewModel
+import edu.timurmakhmutov.bottomnavstrip.lk.LikedAdapter
 
-class StateTripFragment : Fragment(), DrivingSession.DrivingRouteListener, UserLocationObjectListener {
+class StateTripFragment : Fragment(){
+
     private lateinit var fragmentStateTripBinding: FragmentStateTripBinding
-    private lateinit var start:Point
+
+    private lateinit var pathAdapter: LikedAdapter
+    private val model: LKViewModel by activityViewModels()
 
 
     private val tableForDBRepository = TableForDBRepository(Application())
 
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var mapObjects:MapObjectCollection
-    private lateinit var drivingRouter:DrivingRouter
-    private lateinit var drivingSession: DrivingSession
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,9 +37,6 @@ class StateTripFragment : Fragment(), DrivingSession.DrivingRouteListener, UserL
                 fragmentStateTripBinding.root
             ).navigate(R.id.action_stateTripFragment_to_homeFragment)
         }
-        fragmentStateTripBinding.mapview
-        drivingRouter = DirectionsFactory.getInstance().createDrivingRouter()
-        mapObjects = fragmentStateTripBinding.mapview.map.mapObjects.addCollection()
 
 
         return fragmentStateTripBinding.root
@@ -64,98 +44,5 @@ class StateTripFragment : Fragment(), DrivingSession.DrivingRouteListener, UserL
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        MapKitFactory.initialize(context)
-
-        setLocation()
-        racoord()
-
-
-    }
-
-    override fun onStart() {
-        fragmentStateTripBinding.mapview.onStart()
-        MapKitFactory.getInstance().onStart()
-        super.onStart()
-
-    }
-
-    override fun onStop() {
-        fragmentStateTripBinding.mapview.onStart()
-        MapKitFactory.getInstance().onStop()
-        super.onStop()
-
-    }
-    private fun setLocation(){
-        val mapKit: MapKit = MapKitFactory.getInstance()
-        val locationOnMapKit = mapKit.createUserLocationLayer(
-            fragmentStateTripBinding.mapview.mapWindow
-        )
-        locationOnMapKit.isVisible = true
-
-    }
-
-
-    override fun onDrivingRoutes(p0: MutableList<DrivingRoute>) {
-        for (route in p0){
-            mapObjects.addPolyline(route.geometry)
-        }
-    }
-
-    override fun onDrivingRoutesError(p0: Error) {
-        val error = "Неизвестная ошибка"
-        Toast.makeText(context, error, Toast.LENGTH_LONG).show()
-    }
-    @SuppressLint("MissingPermission")
-    private fun racoord(){
-        tableForDBRepository.allPaths.observe(viewLifecycleOwner, Observer { result->
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
-            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-                start = Point(it.latitude, it.longitude)
-                fragmentStateTripBinding.mapview.map.move(
-                    CameraPosition(start, 14.0f, 0.0f, 0.0f),
-                    Animation(Animation.Type.SMOOTH, 0f),
-                    null)
-                val drivingOptions = DrivingOptions()
-                val vehicleOptions = VehicleOptions()
-                val requestPoints:ArrayList<RequestPoint> = ArrayList()
-                requestPoints.add(RequestPoint(start,RequestPointType.WAYPOINT, null))
-                if (result.isNotEmpty()) {
-                    for (item in result) {
-                        val lat = item.lat.toDouble()
-                        val lon = item.lon.toDouble()
-                        requestPoints.add(
-                            RequestPoint(
-                                Point(lat, lon),
-                                RequestPointType.WAYPOINT,
-                                null
-                            )
-                        )
-                        val mark:PlacemarkMapObject = mapObjects.addPlacemark(Point(lat, lon))
-                        mark.opacity = 10000f
-                        mark.setIconStyle(IconStyle())
-                        mark.isDraggable = true
-                    }
-                    drivingSession = drivingRouter.requestRoutes(
-                        requestPoints,
-                        drivingOptions,
-                        vehicleOptions,
-                        this
-                    )
-                }
-            }
-
-        })
-    }
-
-    override fun onObjectAdded(p0: UserLocationView) {
-
-    }
-
-    override fun onObjectRemoved(p0: UserLocationView) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {
-        TODO("Not yet implemented")
     }
 }
